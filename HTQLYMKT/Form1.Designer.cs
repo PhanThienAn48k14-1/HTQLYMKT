25736ae63de70f0e7b955bdea3f98d783f36bd41
PhanThienAn48k14-1 <phanthienan19082004@gmail.com>
2024-11-28 17:31:17 +0700

    Add project files.

---
+++ HTQLYMKT/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace HTQLYMKT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            quảnLíKháchHàngToolStripMenuItem = new ToolStripMenuItem();
+            tạoThêmThôngTinKháchHàngToolStripMenuItem = new ToolStripMenuItem();
+            xemThôngTinKháchHàngToolStripMenuItem = new ToolStripMenuItem();
+            sửaThôngTinKháchHàngToolStripMenuItem = new ToolStripMenuItem();
+            xóaThôngTinKháchHàngToolStripMenuItem = new ToolStripMenuItem();
+            quảnLíNhânViênToolStripMenuItem = new ToolStripMenuItem();
+            tạoThêmThôngTinNhânViênToolStripMenuItem = new ToolStripMenuItem();
+            xemThôngTinNhânViênToolStripMenuItem = new ToolStripMenuItem();
+            sửaThôngTinNhânViênToolStripMenuItem = new ToolStripMenuItem();
+            xóaThôngTinNhânViênToolStripMenuItem = new ToolStripMenuItem();
+            quảnLíTàiKhoảnToolStripMenuItem = new ToolStripMenuItem();
+            tạoThêmTàiKhoảnToolStripMenuItem = new ToolStripMenuItem();
+            xemThôngTinTàiKhoảnToolStripMenuItem = new ToolStripMenuItem();
+            sửaThôngTinTàiKhoảnToolStripMenuItem = new ToolStripMenuItem();
+            xóaThôngTinTàiKhoảnToolStripMenuItem = new ToolStripMenuItem();
+            quảnLíHợpĐồngToolStripMenuItem = new ToolStripMenuItem();
+            tạoHợpĐồngToolStripMenuItem = new ToolStripMenuItem();
+            xemHợpĐồngToolStripMenuItem = new ToolStripMenuItem();
+            sửaHợpĐồngToolStripMenuItem = new ToolStripMenuItem();
+            xóaHợpĐồngToolStripMenuItem = new ToolStripMenuItem();
+            quảnLíDịchVụMarketingToolStripMenuItem = new ToolStripMenuItem();
+            tạoThêmDịchVụMarketingToolStripMenuItem = new ToolStripMenuItem();
+            xemThôngTinDịchVụMarketingToolStripMenuItem = new ToolStripMenuItem();
+            sửaThôngTinDịchVụMarketingToolStripMenuItem = new ToolStripMenuItem();
+            xóaThôngTinDịchVụMarketingToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { quảnLíKháchHàngToolStripMenuItem, quảnLíNhânViênToolStripMenuItem, quảnLíTàiKhoảnToolStripMenuItem, quảnLíHợpĐồngToolStripMenuItem, quảnLíDịchVụMarketingToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // quảnLíKháchHàngToolStripMenuItem
+            // 
+            quảnLíKháchHàngToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { tạoThêmThôngTinKháchHàngToolStripMenuItem, xemThôngTinKháchHàngToolStripMenuItem, sửaThôngTinKháchHàngToolStripMenuItem, xóaThôngTinKháchHàngToolStripMenuItem });
+            quảnLíKháchHàngToolStripMenuItem.Name = "quảnLíKháchHàngToolStripMenuItem";
+            quảnLíKháchHàngToolStripMenuItem.Size = new Size(122, 20);
+            quảnLíKháchHàngToolStripMenuItem.Text = "Quản lí khách hàng";
+            // 
+            // tạoThêmThôngTinKháchHàngToolStripMenuItem
+            // 
+            tạoThêmThôngTinKháchHàngToolStripMenuItem.Name = "tạoThêmThôngTinKháchHàngToolStripMenuItem";
+            tạoThêmThôngTinKháchHàngToolStripMenuItem.Size = new Size(244, 22);
+            tạoThêmThôngTinKháchHàngToolStripMenuItem.Text = "Tạo thêm thông tin khách hàng ";
+            tạoThêmThôngTinKháchHàngToolStripMenuItem.Click += tạoThêmThôngTinKháchHàngToolStripMenuItem_Click;
+            // 
+            // xemThôngTinKháchHàngToolStripMenuItem
+            // 
+            xemThôngTinKháchHàngToolStripMenuItem.Name = "xemThôngTinKháchHàngToolStripMenuItem";
+            xemThôngTinKháchHàngToolStripMenuItem.Size = new Size(244, 22);
+            xemThôngTinKháchHàngToolStripMenuItem.Text = "Xem thông tin khách hàng";
+            xemThôngTinKháchHàngToolStripMenuItem.Click += xemThôngTinKháchHàngToolStripMenuItem_Click;
+            // 
+            // sửaThôngTinKháchHàngToolStripMenuItem
+            // 
+            sửaThôngTinKháchHàngToolStripMenuItem.Name = "sửaThôngTinKháchHàngToolStripMenuItem";
+            sửaThôngTinKháchHàngToolStripMenuItem.Size = new Size(244, 22);
+            sửaThôngTinKháchHàngToolStripMenuItem.Text = "Sửa thông tin khách hàng";
+            sửaThôngTinKháchHàngToolStripMenuItem.Click += sửaThôngTinKháchHàngToolStripMenuItem_Click;
+            // 
+            // xóaThôngTinKháchHàngToolStripMenuItem
+            // 
+            xóaThôngTinKháchHàngToolStripMenuItem.Name = "xóaThôngTinKháchHàngToolStripMenuItem";
+            xóaThôngTinKháchHàngToolStripMenuItem.Size = new Size(244, 22);
+            xóaThôngTinKháchHàngToolStripMenuItem.Text = "Xóa thông tin khách hàng";
+            xóaThôngTinKháchHàngToolStripMenuItem.Click += xóaThôngTinKháchHàngToolStripMenuItem_Click;
+            // 
+            // quảnLíNhânViênToolStripMenuItem
+            // 
+            quảnLíNhânViênToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { tạoThêmThôngTinNhânViênToolStripMenuItem, xemThôngTinNhânViênToolStripMenuItem, sửaThôngTinNhânViênToolStripMenuItem, xóaThôngTinNhânViênToolStripMenuItem });
+            quảnLíNhânViênToolStripMenuItem.Name = "quảnLíNhânViênToolStripMenuItem";
+            quảnLíNhânViênToolStripMenuItem.Size = new Size(112, 20);
+            quảnLíNhânViênToolStripMenuItem.Text = "Quản lí nhân viên";
+            // 
+            // tạoThêmThôngTinNhânViênToolStripMenuItem
+            // 
+            tạoThêmThôngTinNhânViênToolStripMenuItem.Name = "tạoThêmThôngTinNhânViênToolStripMenuItem";
+            tạoThêmThôngTinNhânViênToolStripMenuItem.Size = new Size(231, 22);
+            tạoThêmThôngTinNhânViênToolStripMenuItem.Text = "Tạo thêm thông tin nhân viên";
+            tạoThêmThôngTinNhânViênToolStripMenuItem.Click += tạoThêmThôngTinNhânViênToolStripMenuItem_Click;
+            // 
+            // xemThôngTinNhânViênToolStripMenuItem
+            // 
+            xemThôngTinNhânViênToolStripMenuItem.Name = "xemThôngTinNhânViênToolStripMenuItem";
+            xemThôngTinNhânViênToolStripMenuItem.Size = new Size(231, 22);
+            xemThôngTinNhânViênToolStripMenuItem.Text = "Xem thông tin nhân viên";
+            xemThôngTinNhânViênToolStripMenuItem.Click += xemThôngTinNhânViênToolStripMenuItem_Click;
+            // 
+            // sửaThôngTinNhânViênToolStripMenuItem
+            // 
+            sửaThôngTinNhânViênToolStripMenuItem.Name = "sửaThôngTinNhânViênToolStripMenuItem";
+            sửaThôngTinNhânViênToolStripMenuItem.Size = new Size(231, 22);
+            sửaThôngTinNhânViênToolStripMenuItem.Text = "Sửa thông tin nhân viên";
+            sửaThôngTinNhânViênToolStripMenuItem.Click += sửaThôngTinNhânViênToolStripMenuItem_Click;
+            // 
+            // xóaThôngTinNhânViênToolStripMenuItem
+            // 
+            xóaThôngTinNhânViênToolStripMenuItem.Name = "xóaThôngTinNhânViênToolStripMenuItem";
+            xóaThôngTinNhânViênToolStripMenuItem.Size = new Size(231, 22);
+            xóaThôngTinNhânViênToolStripMenuItem.Text = "Xóa thông tin nhân viên";
+            xóaThôngTinNhânViênToolStripMenuItem.Click += xóaThôngTinNhânViênToolStripMenuItem_Click;
+            // 
+            // quảnLíTàiKhoảnToolStripMenuItem
+            // 
+            quảnLíTàiKhoảnToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { tạoThêmTàiKhoảnToolStripMenuItem, xemThôngTinTàiKhoảnToolStripMenuItem, sửaThôngTinTàiKhoảnToolStripMenuItem, xóaThôngTinTàiKhoảnToolStripMenuItem });
+            quảnLíTàiKhoảnToolStripMenuItem.Name = "quảnLíTàiKhoảnToolStripMenuItem";
+            quảnLíTàiKhoảnToolStripMenuItem.Size = new Size(109, 20);
+            quảnLíTàiKhoảnToolStripMenuItem.Text = "Quản lí tài khoản";
+            // 
+            // tạoThêmTàiKhoảnToolStripMenuItem
+            // 
+            tạoThêmTàiKhoảnToolStripMenuItem.Name = "tạoThêmTàiKhoảnToolStripMenuItem";
+            tạoThêmTàiKhoảnToolStripMenuItem.Size = new Size(202, 22);
+            tạoThêmTàiKhoảnToolStripMenuItem.Text = "Tạo thêm tài khoản";
+            tạoThêmTàiKhoảnToolStripMenuItem.Click += tạoThêmTàiKhoảnToolStripMenuItem_Click;
+            // 
+            // xemThôngTinTàiKhoảnToolStripMenuItem
+            // 
+            xemThôngTinTàiKhoảnToolStripMenuItem.Name = "xemThôngTinTàiKhoảnToolStripMenuItem";
+            xemThôngTinTàiKhoảnToolStripMenuItem.Size = new Size(202, 22);
+            xemThôngTinTàiKhoảnToolStripMenuItem.Text = "Xem thông tin tài khoản";
+            xemThôngTinTàiKhoảnToolStripMenuItem.Click += xemThôngTinTàiKhoảnToolStripMenuItem_Click;
+            // 
+            // sửaThôngTinTàiKhoảnToolStripMenuItem
+            // 
+            sửaThôngTinTàiKhoảnToolStripMenuItem.Name = "sửaThôngTinTàiKhoảnToolStripMenuItem";
+            sửaThôngTinTàiKhoảnToolStripMenuItem.Size = new Size(202, 22);
+            sửaThôngTinTàiKhoảnToolStripMenuItem.Text = "Sửa thông tin tài khoản";
+            sửaThôngTinTàiKhoảnToolStripMenuItem.Click += sửaThôngTinTàiKhoảnToolStripMenuItem_Click;
+            // 
+            // xóaThôngTinTàiKhoảnToolStripMenuItem
+            // 
+            xóaThôngTinTàiKhoảnToolStripMenuItem.Name = "xóaThôngTinTàiKhoảnToolStripMenuItem";
+            xóaThôngTinTàiKhoảnToolStripMenuItem.Size = new Size(202, 22);
+            xóaThôngTinTàiKhoảnToolStripMenuItem.Text = "Xóa thông tin tài khoản";
+            xóaThôngTinTàiKhoảnToolStripMenuItem.Click += xóaThôngTinTàiKhoảnToolStripMenuItem_Click;
+            // 
+            // quảnLíHợpĐồngToolStripMenuItem
+            // 
+            quảnLíHợpĐồngToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { tạoHợpĐồngToolStripMenuItem, xemHợpĐồngToolStripMenuItem, sửaHợpĐồngToolStripMenuItem, xóaHợpĐồngToolStripMenuItem });
+            quảnLíHợpĐồngToolStripMenuItem.Name = "quảnLíHợpĐồngToolStripMenuItem";
+            quảnLíHợpĐồngToolStripMenuItem.Size = new Size(112, 20);
+            quảnLíHợpĐồngToolStripMenuItem.Text = "Quản lí hợp đồng";
+            // 
+            // tạoHợpĐồngToolStripMenuItem
+            // 
+            tạoHợpĐồngToolStripMenuItem.Name = "tạoHợpĐồngToolStripMenuItem";
+            tạoHợpĐồngToolStripMenuItem.Size = new Size(153, 22);
+            tạoHợpĐồngToolStripMenuItem.Text = "Tạo hợp đồng";
+            tạoHợpĐồngToolStripMenuItem.Click += tạoHợpĐồngToolStripMenuItem_Click;
+            // 
+            // xemHợpĐồngToolStripMenuItem
+            // 
+            xemHợpĐồngToolStripMenuItem.Name = "xemHợpĐồngToolStripMenuItem";
+            xemHợpĐồngToolStripMenuItem.Size = new Size(153, 22);
+            xemHợpĐồngToolStripMenuItem.Text = "Xem hợp đồng";
+            xemHợpĐồngToolStripMenuItem.Click += xemHợpĐồngToolStripMenuItem_Click;
+            // 
+            // sửaHợpĐồngToolStripMenuItem
+            // 
+            sửaHợpĐồngToolStripMenuItem.Name = "sửaHợpĐồngToolStripMenuItem";
+            sửaHợpĐồngToolStripMenuItem.Size = new Size(153, 22);
+            sửaHợpĐồngToolStripMenuItem.Text = "Sửa hợp đồng";
+            sửaHợpĐồngToolStripMenuItem.Click += sửaHợpĐồngToolStripMenuItem_Click;
+            // 
+            // xóaHợpĐồngToolStripMenuItem
+            // 
+            xóaHợpĐồngToolStripMenuItem.Name = "xóaHợpĐồngToolStripMenuItem";
+            xóaHợpĐồngToolStripMenuItem.Size = new Size(153, 22);
+            xóaHợpĐồngToolStripMenuItem.Text = "Xóa hợp đồng";
+            xóaHợpĐồngToolStripMenuItem.Click += xóaHợpĐồngToolStripMenuItem_Click;
+            // 
+            // quảnLíDịchVụMarketingToolStripMenuItem
+            // 
+            quảnLíDịchVụMarketingToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { tạoThêmDịchVụMarketingToolStripMenuItem, xemThôngTinDịchVụMarketingToolStripMenuItem, sửaThôngTinDịchVụMarketingToolStripMenuItem, xóaThôngTinDịchVụMarketingToolStripMenuItem });
+            quảnLíDịchVụMarketingToolStripMenuItem.Name = "quảnLíDịchVụMarketingToolStripMenuItem";
+            quảnLíDịchVụMarketingToolStripMenuItem.Size = new Size(156, 20);
+            quảnLíDịchVụMarketingToolStripMenuItem.Text = "Quản lí dịch vụ marketing";
+            // 
+            // tạoThêmDịchVụMarketingToolStripMenuItem
+            // 
+            tạoThêmDịchVụMarketingToolStripMenuItem.Name = "tạoThêmDịchVụMarketingToolStripMenuItem";
+            tạoThêmDịchVụMarketingToolStripMenuItem.Size = new Size(249, 22);
+            tạoThêmDịchVụMarketingToolStripMenuItem.Text = "Tạo thêm dịch vụ marketing";
+            tạoThêmDịchVụMarketingToolStripMenuItem.Click += tạoThêmDịchVụMarketingToolStripMenuItem_Click;
+            // 
+            // xemThôngTinDịchVụMarketingToolStripMenuItem
+            // 
+            xemThôngTinDịchVụMarketingToolStripMenuItem.Name = "xemThôngTinDịchVụMarketingToolStripMenuItem";
+            xemThôngTinDịchVụMarketingToolStripMenuItem.Size = new Size(249, 22);
+            xemThôngTinDịchVụMarketingToolStripMenuItem.Text = "Xem thông tin dịch vụ marketing";
+            xemThôngTinDịchVụMarketingToolStripMenuItem.Click += xemThôngTinDịchVụMarketingToolStripMenuItem_Click;
+            // 
+            // sửaThôngTinDịchVụMarketingToolStripMenuItem
+            // 
+            sửaThôngTinDịchVụMarketingToolStripMenuItem.Name = "sửaThôngTinDịchVụMarketingToolStripMenuItem";
+            sửaThôngTinDịchVụMarketingToolStripMenuItem.Size = new Size(249, 22);
+            sửaThôngTinDịchVụMarketingToolStripMenuItem.Text = "Sửa thông tin dịch vụ marketing";
+            sửaThôngTinDịchVụMarketingToolStripMenuItem.Click += sửaThôngTinDịchVụMarketingToolStripMenuItem_Click;
+            // 
+            // xóaThôngTinDịchVụMarketingToolStripMenuItem
+            // 
+            xóaThôngTinDịchVụMarketingToolStripMenuItem.Name = "xóaThôngTinDịchVụMarketingToolStripMenuItem";
+            xóaThôngTinDịchVụMarketingToolStripMenuItem.Size = new Size(249, 22);
+            xóaThôngTinDịchVụMarketingToolStripMenuItem.Text = "Xóa thông tin dịch vụ marketing";
+            xóaThôngTinDịchVụMarketingToolStripMenuItem.Click += xóaThôngTinDịchVụMarketingToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "HTQLMKT";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem quảnLíKháchHàngToolStripMenuItem;
+        private ToolStripMenuItem quảnLíNhânViênToolStripMenuItem;
+        private ToolStripMenuItem quảnLíTàiKhoảnToolStripMenuItem;
+        private ToolStripMenuItem quảnLíHợpĐồngToolStripMenuItem;
+        private ToolStripMenuItem quảnLíDịchVụMarketingToolStripMenuItem;
+        private ToolStripMenuItem tạoThêmThôngTinKháchHàngToolStripMenuItem;
+        private ToolStripMenuItem xemThôngTinKháchHàngToolStripMenuItem;
+        private ToolStripMenuItem sửaThôngTinKháchHàngToolStripMenuItem;
+        private ToolStripMenuItem xóaThôngTinKháchHàngToolStripMenuItem;
+        private ToolStripMenuItem tạoThêmThôngTinNhânViênToolStripMenuItem;
+        private ToolStripMenuItem xemThôngTinNhânViênToolStripMenuItem;
+        private ToolStripMenuItem sửaThôngTinNhânViênToolStripMenuItem;
+        private ToolStripMenuItem xóaThôngTinNhânViênToolStripMenuItem;
+        private ToolStripMenuItem tạoThêmTàiKhoảnToolStripMenuItem;
+        private ToolStripMenuItem xemThôngTinTàiKhoảnToolStripMenuItem;
+        private ToolStripMenuItem sửaThôngTinTàiKhoảnToolStripMenuItem;
+        private ToolStripMenuItem xóaThôngTinTàiKhoảnToolStripMenuItem;
+        private ToolStripMenuItem tạoHợpĐồngToolStripMenuItem;
+        private ToolStripMenuItem xemHợpĐồngToolStripMenuItem;
+        private ToolStripMenuItem sửaHợpĐồngToolStripMenuItem;
+        private ToolStripMenuItem xóaHợpĐồngToolStripMenuItem;
+        private ToolStripMenuItem tạoThêmDịchVụMarketingToolStripMenuItem;
+        private ToolStripMenuItem xemThôngTinDịchVụMarketingToolStripMenuItem;
+        private ToolStripMenuItem sửaThôngTinDịchVụMarketingToolStripMenuItem;
+        private ToolStripMenuItem xóaThôngTinDịchVụMarketingToolStripMenuItem;
+    }
+}
---
+++ HTQLYMKT/Form1.cs
@@ -0,0 +1,150 @@
+﻿namespace HTQLYMKT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tạoThêmThôngTinKháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CQLKH cQLKH = new CQLKH();
+            cQLKH.MdiParent = this;
+            cQLKH.Show();
+        }
+
+        private void xemThôngTinKháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RQLKH rQLKH = new RQLKH();
+            rQLKH.MdiParent = this;
+            rQLKH.Show();
+        }
+
+        private void sửaThôngTinKháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UQLKH uQLKH = new UQLKH();
+            uQLKH.MdiParent = this;
+            uQLKH.Show();
+        }
+
+        private void xóaThôngTinKháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DQLKH dQLKH = new DQLKH();
+            dQLKH.MdiParent = this;
+            dQLKH.Show();
+        }
+
+        private void tạoThêmThôngTinNhânViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CQLNV cQLNV = new CQLNV();
+            cQLNV.MdiParent = this;
+            cQLNV.Show();
+        }
+
+        private void xemThôngTinNhânViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RQLNV rQLNV = new RQLNV();
+            rQLNV.MdiParent = this;
+            rQLNV.Show();
+        }
+
+        private void sửaThôngTinNhânViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UQLNV uQLNV = new UQLNV();
+            uQLNV.MdiParent = this;
+            uQLNV.Show();
+        }
+
+        private void xóaThôngTinNhânViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DQLNV dQLNV = new DQLNV();
+            dQLNV.MdiParent = this;
+            dQLNV.Show();
+        }
+
+        private void tạoThêmTàiKhoảnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CQLTK cQLTK = new CQLTK();
+            cQLTK.MdiParent = this;
+            cQLTK.Show();
+        }
+
+        private void xemThôngTinTàiKhoảnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RQLTK rqLTK = new RQLTK();
+            rqLTK.MdiParent = this;
+            rqLTK.Show();
+        }
+
+        private void sửaThôngTinTàiKhoảnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UQLTK uQLTK = new UQLTK();
+            uQLTK.MdiParent = this;
+            uQLTK.Show();
+        }
+
+        private void xóaThôngTinTàiKhoảnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DQLTK dQLTK = new DQLTK();
+            dQLTK.MdiParent = this;
+            dQLTK.Show();
+        }
+
+        private void tạoHợpĐồngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CQLHD cQLHD = new CQLHD();
+            cQLHD.MdiParent = this;
+            cQLHD.Show();
+        }
+
+        private void xemHợpĐồngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RQLHD rQLHD = new RQLHD();
+            rQLHD.MdiParent = this;
+            rQLHD.Show();
+        }
+
+        private void sửaHợpĐồngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UQLHD uQLHD = new UQLHD();
+            uQLHD.MdiParent = this;
+            uQLHD.Show();
+        }
+
+        private void xóaHợpĐồngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DQLHD dQLHD = new DQLHD();
+            dQLHD.MdiParent = this;
+            dQLHD.Show();
+        }
+
+        private void tạoThêmDịchVụMarketingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CQLDV cQLDV = new CQLDV();
+            cQLDV.MdiParent = this;
+            cQLDV.Show();
+        }
+
+        private void xemThôngTinDịchVụMarketingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RQLDV rQLDV = new RQLDV();
+            rQLDV.MdiParent = this;
+            rQLDV.Show();
+        }
+
+        private void sửaThôngTinDịchVụMarketingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UQLDV uQLDV = new UQLDV();
+            uQLDV.MdiParent = this;
+            uQLDV.Show();
+        }
+
+        private void xóaThôngTinDịchVụMarketingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DQLDV dQLDV = new DQLDV();
+            dQLDV.MdiParent = this;
+            dQLDV.Show();
+        }
+    }
+}
